befffdc8a9de581b01436e6919620c351f74eb9c
Low Kian Seong <kianseong@gmail.com>
2015-03-31 17:25:16 +0800

    Update setup.py
    Adding `description` to `setup.py`

--- setup.py
+++ setup.py
@@ -103,7 +103,7 @@ requires.append(
 setup(
     name='%s_%s' % (PREFIX, MODULE),
     version=info.get('version', '0.0.1'),
-    description="",
+    description="Openlab Sale Channel",
     author="Openlabs Technologies and Consulting (P) Ltd.",
     author_email='info@openlabs.co.in',
     url='http://www.openlabs.co.in/',
